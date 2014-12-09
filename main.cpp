ad1dc7bfb5c31c8611163fe9083c6a743e4d4160
Conor Brady <conorjbrady1@gmail.com>
2014-12-09 21:34:18 +0000

    Working pretty good

--- main.cpp
+++ main.cpp
@@ -29,7 +29,47 @@ string type2str(int type) {
 	return r;
 }
 
+void printImage(Mat chamfer_image) {
 
+	for(int i = 0; i < chamfer_image.rows; i++) {
+		for(int j = 0; j < chamfer_image.cols; j++) {
+			float val = chamfer_image.at<float>(i, j);
+			if(val == 0) {
+
+				printf("\x1b[31m0\x1b[0m ") ;
+			} else {
+				printf("1 ") ;
+			}
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+vector<float> maxes(9);
+vector<float> mins(9);
+
+float chamfer_match(vector<Point> templatePoints, Mat chamferedImage, Size matchingSpace) {
+
+	assert(chamferedImage.type() == CV_32FC1);
+	float min = -1;
+	for(int j = 0; j < matchingSpace.width; j++) {
+		for(int k = 0; k < matchingSpace.height; k++) {
+			float newValue = 0;
+			for(int i = 0; i < templatePoints.size(); i ++ ) {
+				newValue += chamferedImage.at<float>(templatePoints[i]+Point(j,k));
+			}
+			if(min==-1) {
+				min = newValue;
+			} else {
+				min = fmin(newValue,min);
+			}
+		}
+	}
+
+	return min/(templatePoints.size()-400);
+}
 
 int main( int, char** argv )
 {
@@ -54,15 +94,34 @@ int main( int, char** argv )
 		"known_signs/yield.png"
 	};
 
-	Mat src; Mat hsv;
-	Mat mask;
-
 	vector<Mat> knownSigns(knownSignFiles.size());
-	vector<Mat> grayKnownSigns;
+	vector<vector<Point>> knownSignsTemplatePoints;
+
 	for (int i = 0; i < knownSignFiles.size(); i++) {
+		maxes[i] = 0;
+		mins[i] = 10000000;
 		knownSigns[i] = imread(knownSignFiles[i]);
+
+		Mat temp;
+
+		cvtColor(knownSigns[i],temp,CV_BGR2GRAY);
+		Canny(temp,temp,100,200);
+
+		knownSignsTemplatePoints.push_back(vector<Point>());
+
+		for(int j = 0; j < knownSigns[i].rows; j++) {
+			for(int k = 0; k < knownSigns[i].cols; k++) {
+				if(temp.at<unsigned char>(j,k) != 0) {
+					knownSignsTemplatePoints[i].push_back(Point(k,j));
+				}
+			}
+		}
+		imshow("bestmatch",temp);
+		// waitKey(0);
 	}
 
+	Mat src; Mat hsv;
+	Mat mask;
 
 	src = imread( "back_project_data.png", 1 );
 	cvtColor( src, hsv, COLOR_BGR2HSV );
@@ -80,9 +139,7 @@ int main( int, char** argv )
 	for(int i = 0; i < unknownSignFiles.size(); i++) {
 		Mat image = imread(unknownSignFiles[i]);
 		cvtColor( image, hsv, COLOR_BGR2HSV );
-		Mat invGrayImage;
-		cvtColor( image, invGrayImage, CV_BGR2GRAY);
-		bitwise_not(invGrayImage,invGrayImage);
+
 		Mat backproj;
 		calcBackProject( &hsv, 1, channels, hist, backproj, ranges, 1, true );
 		dilate(backproj,backproj,Mat());
@@ -104,63 +161,53 @@ int main( int, char** argv )
 				drawContours(mask, contours, j, Scalar(255), CV_FILLED, 8, hierarchy, 0);
 
 				Mat maskedImage;
-
+				Mat invGrayImage;
+				cvtColor( image, invGrayImage, CV_BGR2GRAY);
+				bitwise_not(invGrayImage,invGrayImage);
 				invGrayImage.copyTo(maskedImage, mask);
 				Mat searchImage(maskedImage, bounds);
 				bitwise_not(maskedImage,maskedImage);
 
-				// Scalar colour( rand()&0xFF,rand()&0xFF,rand()&0xFF );
-				// drawContours( image, contours, i, colour, CV_FILLED, 8, hierarchy );
-				// rectangle(image,boundRects[boundRects.size()-1],colour,3);
-				// Mat searchImage = Mat(image,boundsRects[boundRects.size()-1]);
-				// imshow("colorsearch", searchImage);
-				resize(searchImage,searchImage,Size(75,75));
-				copyMakeBorder( searchImage, searchImage, 20, 20, 30, 30, BORDER_CONSTANT, Scalar::all(255) );
+				resize(searchImage,searchImage,Size(80,80));
+				copyMakeBorder( searchImage, searchImage, 15, 15, 20, 20, BORDER_CONSTANT, Scalar::all(255) );
 
 				Mat edge_image;
 				Mat chamfer_image;
 				Canny( searchImage, edge_image, 50, 100);
-				threshold( edge_image, edge_image, 127, 255, THRESH_BINARY_INV );
+				threshold( edge_image, chamfer_image, 127, 255, THRESH_BINARY_INV );
+				imshow("edges",edge_image);
+				distanceTransform( chamfer_image, chamfer_image, CV_DIST_L2, 3);
 
-				distanceTransform( edge_image, chamfer_image, CV_DIST_L2, 3);
-				//normalize(chamfer_image, chamfer_image, 0.0, 1.0, NORM_MINMAX, CV_8U);
-				//chamfer_image.convertTo(chamfer_image, CV_8U);
+				// chamfer_image.convertTo(chamfer_image, CV_8U);
 				//
 				imshow("searchimage",searchImage);
-				//
+
 				int closest = -1;
 				double closestVal = 1000000;
 				for(int k = 0; k < knownSigns.size(); k++) {
-					float bestForSign = 0;
-					Mat matching_space;
-					matching_space.create(searchImage.cols-knownSigns[k].cols+1,searchImage.rows-knownSigns[k].rows+1, CV_8U );
 
-					Mat temp;
-					Mat matcher;
-
-					cvtColor(knownSigns[k],matcher,CV_BGR2GRAY);
-					Canny(matcher,matcher,100,200);
-
-					matchTemplate( chamfer_image, matcher, matching_space, CV_TM_CCORR_NORMED );
-					double min, max;
-					minMaxLoc(matching_space,&min,&max);
-					if( min < bestForSign ) {
-						bestForSign = min;
-					}
-
-
-
-					if(bestForSign < closestVal) {
+					float match = chamfer_match(knownSignsTemplatePoints[k],chamfer_image,Size(chamfer_image.size().width-knownSigns[i].size().width,chamfer_image.size().height-knownSigns[i].size().height));
+					maxes[k] = fmax(maxes[k],match);
+					mins[k] = fmin(mins[k],match);
+					//float backwardsMatch = chamfer_match(edge_image,chamferedKnownSigns[k]);
+					cout << "Image " << knownSignFiles[k] << ": " << match << endl;
+					// match += backwardsMatch;
+					if(match < closestVal) {
 						closest = k;
-						closestVal = bestForSign;
+						closestVal = match;
 					}
 				}
 				cout << closestVal << endl;
+				cout << closest << endl;
 				imshow("bestmatch",knownSigns[closest]);
 				waitKey(0);
 
 			}
 		}
+
+	}
+	for (int i = 0; i < knownSigns.size(); i++) {
+		cout << i << " Max: " << maxes[i] << " Min: " << mins[i] << " Size: " << knownSignsTemplatePoints[i].size() << endl;
 	}
 	return 0;
 }
